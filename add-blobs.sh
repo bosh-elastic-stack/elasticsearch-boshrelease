5cd92b3f83a09f13712d0cc98e1a4b136328e999
Toshiaki Maki <tmaki@pivotal.io>
2018-07-15 02:50:16 +0900

    Bump to Elasticsearch 6.3.1

--- add-blobs.sh
+++ add-blobs.sh
@@ -13,9 +13,9 @@ if [ ! -f ${DIR}/blobs/java/openjdk-1.8.0_172.tar.gz ];then
     bosh add-blob --dir=${DIR} openjdk-1.8.0_172.tar.gz java/openjdk-1.8.0_172.tar.gz
 fi
 
-if [ ! -f ${DIR}/blobs/elasticsearch/elasticsearch-6.2.4.tar.gz ];then
-    curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.tar.gz
-    bosh add-blob --dir=${DIR} elasticsearch-6.2.4.tar.gz elasticsearch/elasticsearch-6.2.4.tar.gz
+if [ ! -f ${DIR}/blobs/elasticsearch/elasticsearch-6.3.1.tar.gz ];then
+    curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.1.tar.gz
+    bosh add-blob --dir=${DIR} elasticsearch-6.3.1.tar.gz elasticsearch/elasticsearch-6.3.1.tar.gz
 fi
 
 cd -

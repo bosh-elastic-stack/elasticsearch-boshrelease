ee8f37d99cdae2aa3e109a5d6c878f45f5b789c4
Toshiaki Maki <tmaki@pivotal.io>
2018-07-14 22:40:54 +0900

    Bump to JDK 1.8.0_172

--- add-blobs.sh
+++ add-blobs.sh
@@ -8,9 +8,9 @@ cd .downloads
 
 
 
-if [ ! -f ${DIR}/blobs/java/openjdk-1.8.0_162.tar.gz ];then
-    curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_162.tar.gz
-    bosh add-blob --dir=${DIR} openjdk-1.8.0_162.tar.gz java/openjdk-1.8.0_162.tar.gz
+if [ ! -f ${DIR}/blobs/java/openjdk-1.8.0_172.tar.gz ];then
+    curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_172.tar.gz
+    bosh add-blob --dir=${DIR} openjdk-1.8.0_172.tar.gz java/openjdk-1.8.0_172.tar.gz
 fi
 
 if [ ! -f ${DIR}/blobs/elasticsearch/elasticsearch-6.2.4.tar.gz ];then

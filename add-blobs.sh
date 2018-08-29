ab37ad762d77be81d72a2152b9a9dada203af57b
Toshiaki Maki <tmaki@pivotal.io>
2018-08-30 03:47:51 +0900

    Use OpenJDK BOSH Release

--- add-blobs.sh
+++ add-blobs.sh
@@ -8,11 +8,6 @@ cd .downloads
 
 ES_VERSION=6.4.0
 
-if [ ! -f ${DIR}/blobs/java/openjdk-1.8.0_172.tar.gz ];then
-    curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_172.tar.gz
-    bosh add-blob --dir=${DIR} openjdk-1.8.0_172.tar.gz java/openjdk-1.8.0_172.tar.gz
-fi
-
 if [ ! -f ${DIR}/blobs/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz ];then
     curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
     bosh add-blob --dir=${DIR} elasticsearch-${ES_VERSION}.tar.gz elasticsearch/elasticsearch-${ES_VERSION}.tar.gz

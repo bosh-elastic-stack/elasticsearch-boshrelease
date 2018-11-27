69416d3a628ed0171125e338262ca2f292a3b7d9
Toshiaki Maki <tmaki@pivotal.io>
2018-11-27 14:51:04 +0900

    Consume elasticsearch link in elasticsearch-index-templates errand

--- jobs/elasticsearch-index-templates/templates/bin/run.sh
+++ jobs/elasticsearch-index-templates/templates/bin/run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+<%
+  elasticsearch_host = p("elasticsearch.host")
+  if p("elasticsearch.prefer_bosh_link") then
+      if_link("elasticsearch") { |elasticsearch_link| elasticsearch_host = elasticsearch_link.instances[0].address }
+  end
+
+  elasticsearch_url = p("elasticsearch.protocol") + '://' + elasticsearch_host + ':' + p("elasticsearch.port")
+%>
 
 # If a command fails, exit immediately
 set -e
 
 <% p("elasticsearch.index.template").each do |template| name, body = template.first %>
-	curl -X PUT "localhost:9200/_template/<%= name %>" -H 'Content-Type: application/json' --data-binary '<%= body %>'
+	curl -k -X PUT "<%= elasticsearch_url %>/_template/<%= name %>" -H 'Content-Type: application/json' --data-binary '<%= body %>'
 <% end %>

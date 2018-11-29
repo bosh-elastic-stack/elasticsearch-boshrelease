4b77670f900df7eb3a1b3b8cb193a21cd1ed625a
antoniocascais <antonio.cascais@springer.com>
2018-11-29 18:08:58 +0100

    Added a job to change the dynamic settings of ES (#6)
    With this job it is now possible to change ES properties and save those
    changes along with your bosh deployment. This means that you can store
    your settings changes and have an immutable deployment.
    
    This errand job will make a call to the `_cluster/settings` API with the
    settings that we want to change.
    
    README was updated to include an example.

---
+++ jobs/elasticsearch-dynamic-properties/templates/bin/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+<%
+  elasticsearch_host = p("elasticsearch.host")
+  if p("elasticsearch.prefer_bosh_link") then
+      if_link("elasticsearch") { |elasticsearch_link| elasticsearch_host = elasticsearch_link.instances[0].address }
+  end
+
+  elasticsearch_url = p("elasticsearch.protocol") + '://' + elasticsearch_host + ':' + p("elasticsearch.port")
+%>
+
+# If a command fails, exit immediately
+set -e
+
+curl -k -X PUT "<%= elasticsearch_url %>/_cluster/settings" -H 'Content-Type: application/json' --data-binary '<%=p('elasticsearch.dynamic.properties') %>'

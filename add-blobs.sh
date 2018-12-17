eae0f8d71cc4638e5b33648c614e1d680b9b5cc6
Toshiaki Maki <tmaki@pivotal.io>
2018-12-18 03:25:52 +0900

    Add repository-s3, repository-gcs, repository-azure plugins fixes #7

--- add-blobs.sh
+++ add-blobs.sh
@@ -18,4 +18,20 @@ if [ ! -f ${DIR}/blobs/analysis-kuromoji/analysis-kuromoji-${ES_VERSION}.zip ];t
     bosh add-blob --dir=${DIR} analysis-kuromoji-${ES_VERSION}.zip analysis-kuromoji/analysis-kuromoji-${ES_VERSION}.zip
 fi
 
+if [ ! -f ${DIR}/blobs/repository-s3/repository-s3-${ES_VERSION}.zip ];then
+    curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch-plugins/repository-s3/repository-s3-${ES_VERSION}.zip
+    bosh add-blob --dir=${DIR} repository-s3-${ES_VERSION}.zip repository-s3/repository-s3-${ES_VERSION}.zip
+fi
+
+if [ ! -f ${DIR}/blobs/repository-gcs/repository-gcs-${ES_VERSION}.zip ];then
+    curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch-plugins/repository-gcs/repository-gcs-${ES_VERSION}.zip
+    bosh add-blob --dir=${DIR} repository-gcs-${ES_VERSION}.zip repository-gcs/repository-gcs-${ES_VERSION}.zip
+fi
+
+if [ ! -f ${DIR}/blobs/repository-azure/repository-azure-${ES_VERSION}.zip ];then
+    curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch-plugins/repository-azure/repository-azure-${ES_VERSION}.zip
+    bosh add-blob --dir=${DIR} repository-azure-${ES_VERSION}.zip repository-azure/repository-azure-${ES_VERSION}.zip
+fi
+
+
 cd -

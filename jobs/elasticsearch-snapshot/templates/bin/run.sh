2bf70ffb5819c0d968f70b5c5e188a808b0c5987
Toshiaki Maki <tmaki@pivotal.io>
2019-03-02 03:10:56 +0900

    Add elasticsearch-snapshot errand

---
+++ jobs/elasticsearch-snapshot/templates/bin/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+<%
+  elasticsearch_host = p("elasticsearch.host")
+  if p("elasticsearch.prefer_bosh_link") then
+      if_link("elasticsearch") { |elasticsearch_link| elasticsearch_host = elasticsearch_link.instances[0].address }
+  end
+
+  elasticsearch_url = p("elasticsearch.protocol") + '://' + elasticsearch_host + ':' + p("elasticsearch.port")
+%>
+
+# If a command fails, exit immediately
+set -e
+
+curl -k -s -X PUT "<%= elasticsearch_url %>/_snapshot/<%= p('elasticsearch.snapshots.repository') %>" \
+  -X PUT -H "Content-Type: application/json" \
+  -d '{"type": "<%= p('elasticsearch.snapshots.type') %>", "settings": <%= p('elasticsearch.snapshots.settings').to_json %>}'
---
+++ spec/jobs/elasticsearch-snapshot_spec.rb
@@ -0,0 +1,56 @@
+require 'rspec'
+require 'json'
+require 'bosh/template/test'
+
+describe 'elasticsearch-snapshot job' do
+  let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../..')) }
+  let(:job) { release.job('elasticsearch-snapshot') }
+
+  describe 'run' do
+    let(:template) { job.template('bin/run') }
+    let(:links) { [
+        Bosh::Template::Test::Link.new(
+          name: 'elasticsearch',
+          instances: [Bosh::Template::Test::LinkInstance.new(address: '10.0.8.2')],
+          properties: {
+            'elasticsearch'=> {
+              'cluster_name' => 'test'
+            },
+          }
+        )
+      ] }
+
+    it 'type s3 works' do
+      run = template.render({'elasticsearch' => {
+        'snapshots' => {
+          'repository' => 'foo',
+          'type' => 's3',
+          'settings' => {
+            'bucket' => 'my-s3-bucket',
+            'endpoint' => 'https://s3-ap-northeast-1.amazonaws.com',
+            'access_key' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            'secret_key' => 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy' 
+          }
+        }
+      }}, consumes: links).strip
+      expect(run).to include('curl -k -s -X PUT "http://10.0.8.2:9200/_snapshot/foo"')
+      expect(run).to include('{"type": "s3", "settings": {"bucket":"my-s3-bucket","endpoint":"https://s3-ap-northeast-1.amazonaws.com","access_key":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx","secret_key":"yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"}}')
+    end
+
+    it 'type gcs works' do
+      run = template.render({'elasticsearch' => {
+        'snapshots' => {
+          'repository' => 'foo',
+          'type' => 'gcs',
+          'settings' => {
+            'bucket' => 'my-gcs-bucket',
+            'client' => 'foo',
+            'base_path' => 'dev'
+          }
+        }
+      }}, consumes: links).strip
+      expect(run).to include('curl -k -s -X PUT "http://10.0.8.2:9200/_snapshot/foo"')
+      expect(run).to include('{"type": "gcs", "settings": {"bucket":"my-gcs-bucket","client":"foo","base_path":"dev"}}')
+    end
+  end
+end

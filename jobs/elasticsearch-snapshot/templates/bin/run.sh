8c35b7535ca3da78a223973590d92be63cb687f7
Toshiaki Maki <tmaki@pivotal.io>
2019-03-02 04:16:26 +0900

    Add execution snapshot command

--- jobs/elasticsearch-snapshot/templates/bin/run.sh
+++ jobs/elasticsearch-snapshot/templates/bin/run.sh
@@ -11,6 +11,8 @@
 # If a command fails, exit immediately
 set -e
 
-curl -D /dev/stderr -k -s -X PUT "<%= elasticsearch_url %>/_snapshot/<%= p('elasticsearch.snapshots.repository') %>" \
+curl -D /dev/stderr -k -s -X PUT "<%= elasticsearch_url %>/_snapshot/<%= p('elasticsearch.snapshots.repository') %>?pretty" \
   -X PUT -H "Content-Type: application/json" \
-  -d '{"type": "<%= p('elasticsearch.snapshots.type') %>", "settings": <%= p('elasticsearch.snapshots.settings').to_json %>}'
+  -d '{"type": "<%= p('elasticsearch.snapshots.type') %>", "settings": <%= p('elasticsearch.snapshots.settings').to_json %>}' \
+
+curl -D /dev/stderr -k -s -X PUT "<%= elasticsearch_url %>/_snapshot/<%= p('elasticsearch.snapshots.repository') %>/$(date +%Y%m%d_%H%M%S_%Z  | tr "[:upper:]" "[:lower:]")?wait_for_completion=true&pretty"
